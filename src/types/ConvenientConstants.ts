730629730c36f2f439cc66ba25c12b8d08e786b2
Noah Overcash <novercash@ebsco.com>
2024-02-06 13:06:28 -0500

    fix weird object structuring

--- src/types/ConvenientConstants.ts
+++ src/types/ConvenientConstants.ts
@@ -1,15 +1,20 @@
 import { DataTokenType, HeaderFooterTokenType } from './TokenTypes';
 
+// there are overlap between the HeaderFooterTokenType and DataTokenType, but we want to
+// be explicit that we are considering/including both here.
 export default {
-  [HeaderFooterTokenType.NEWLINE]: '\n',
-  [HeaderFooterTokenType.NEWLINE_MICROSOFT]: '\r\n',
-  [HeaderFooterTokenType.TAB]: '\t',
-  [HeaderFooterTokenType.COMMA]: ',',
-  [HeaderFooterTokenType.SPACE]: ' ',
-
-  [DataTokenType.NEWLINE]: '\n',
-  [DataTokenType.NEWLINE_MICROSOFT]: '\r\n',
-  [DataTokenType.TAB]: '\t',
-  [DataTokenType.COMMA]: ',',
-  [DataTokenType.SPACE]: ' ',
+  ...{
+    [HeaderFooterTokenType.NEWLINE]: '\n',
+    [HeaderFooterTokenType.NEWLINE_MICROSOFT]: '\r\n',
+    [HeaderFooterTokenType.TAB]: '\t',
+    [HeaderFooterTokenType.COMMA]: ',',
+    [HeaderFooterTokenType.SPACE]: ' ',
+  },
+  ...{
+    [DataTokenType.NEWLINE]: '\n',
+    [DataTokenType.NEWLINE_MICROSOFT]: '\r\n',
+    [DataTokenType.TAB]: '\t',
+    [DataTokenType.COMMA]: ',',
+    [DataTokenType.SPACE]: ' ',
+  },
 };
